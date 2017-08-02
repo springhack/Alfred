0be997fe51eae2f8e62b3f4d69d1394ffb1c713a
chenfuchun <chenfuchun@bytedance.com>
2017-08-02 19:42:39 +0800

    Init
    Change-Id: I74d01c2bde325769e3ab4b91c95ba2ccdde87724

---
+++ Alfred.alfredpreferences/workflows/user.workflow.90F48E39-7501-4DA3-9C38-288A0199EACC/config.php
@@ -0,0 +1,7 @@
+<?php
+
+// JIRA系统域名
+$jiraUrl    = 'https://jira.bytedance.com';
+
+// 登陆JIRA后，将Cookie中的‘seraph.rememberme.cookie’值，替换下面的xxx
+$jiraCookie = 'seraph.rememberme.cookie=21363%3Aa24e1c1932357fd360b7083919c7bb8ed7537ee7';
